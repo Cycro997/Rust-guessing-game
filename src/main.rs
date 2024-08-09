d369b44a917705cd82bcaa50b9b814469994dd43
Cycro997 <aaserasser1@gmail.com>
2024-08-10 00:54:54 +0300

    Version 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
-static mut MAX_VALUE: u16 = 0;
+static mut MAX_VALUE: u16 = 10;
+static mut HINT_RANGE: u8 = 100; 
 
 fn set_difficulty(diff: u8){
     println!("DIFFICULTY: {diff}");
@@ -18,47 +19,86 @@ fn set_difficulty(diff: u8){
             8 => {MAX_VALUE = 60;},
             9 => {MAX_VALUE = 80;},
             10 => {MAX_VALUE = 100;},
-            _ => {}
+            11 => {MAX_VALUE = 125;},
+            12 => {MAX_VALUE = 160;},
+            13 => {MAX_VALUE = 200;},
+            14 => {MAX_VALUE = 250;},
+            15 => {MAX_VALUE = 325;},
+            _ => {panic!("Invalid value")}
+        }
+        if diff <= 5{
+            HINT_RANGE = 100;
+        } else if diff <= 7{
+            HINT_RANGE = 75;
+        } else if diff <= 10{
+            HINT_RANGE = 60;
+        } else if diff <= 13{
+            HINT_RANGE = 45;
+        } else if diff <= 15{
+            HINT_RANGE = 35;
         }
     }
 }
 
 fn main(){
-    let mut diff: String = String::new(); // difficulty
+    let mut diff: String; // difficulty
     
-    println!("Please enter a difficulty name or level between 1 and 10");
+    println!("Please enter a difficulty name or level between 1 and 15");
     println!("Difficulty names: [effortless: 1, easy: 2, mild: 3, moderate: 4, ");
     println!("medium: 5, average: 6, above average: 7, talented: 8");
-    println!("spicy: 9, hard: 10]");
+    println!("spicy: 9, hard: 10, difficult: 11, painful: 12, extreme: 13");
+    println!("insane: 14, destructive: 15]");
 
-    io::stdin()
-        .read_line(&mut diff)
-        .expect("Failed to read line");
+    loop{
+        diff = String::new();
 
-    match &*(diff.trim()) {
-        "easy" => set_difficulty(1),
-        _ => {
-            if !diff.trim().chars().all(char::is_numeric){
-                panic!("Nuh uh");
-            }
-            set_difficulty(
-                diff.trim().parse().expect(
-                    "unexpected behavior alert"
+        io::stdin()
+            .read_line(&mut diff)
+            .expect("Failed to read line");
+
+        match &*(diff.trim()) {
+            "effortless" => set_difficulty(1),
+            "easy" => set_difficulty(2),
+            "mild" => set_difficulty(3),
+            "moderate" => set_difficulty(4),
+            "medium" => set_difficulty(5),
+            "average" => set_difficulty(6),
+            "above average" => set_difficulty(7),
+            "talented" => set_difficulty(8),
+            "spicy" => set_difficulty(9),
+            "hard" => set_difficulty(10),
+            "difficult" => set_difficulty(11),
+            "painful" => set_difficulty(12),
+            "extreme" => set_difficulty(13),
+            "insane" => set_difficulty(14),
+            "destructive" => set_difficulty(15),
+            _ => {
+                if !diff.trim().chars().all(char::is_numeric){
+                    println!("Please enter a valid difficulty");
+                    continue;
+                }
+                set_difficulty(
+                    diff.trim().parse().expect(
+                        "unexpected behavior alert"
+                    )
                 )
-            )
+            }
         }
-    }    
-    let max_value: u16;
-    unsafe{
-        max_value = MAX_VALUE
+        break;
     }
+    let max_value: u16;
+    let hint_range: u8;
+    unsafe{
+        max_value = MAX_VALUE;
+        hint_range = HINT_RANGE;
+    }
+    let hint_range_num: u16 = 
+        (max_value as f32 * ((hint_range as f32 / 100.))) as u16;
 
     println!("Guess the number");
     println!("Please input a number between 1 and {max_value}");
 
     let number: u16;
-
-    
 
     number = rand::thread_rng().gen_range(1..=max_value);
 
@@ -69,13 +109,38 @@ fn main(){
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u16 = guess.trim().parse().expect("Please type a number!");
+
+        let guess: u16 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a number");
+                continue;
+            },
+        };
 
         println!("You guessed: {}", guess);
+        
+        let difference: u16 = (
+            guess as i16 - number as i16
+        ).abs() as u16;
 
         match guess.cmp(&number) {
-            Ordering::Less => {println!("Too small!"); continue;},
-            Ordering::Greater => {println!("Too big!"); continue;},
+            Ordering::Less => {
+                if difference <= hint_range_num{
+                    println!("Too small!");
+                } else{
+                    println!("Wrong number");
+                }
+                continue;
+            },
+            Ordering::Greater => {
+                if difference <= hint_range_num{
+                    println!("Too big!");
+                } else{
+                    println!("Wrong number");
+                }
+                continue;
+            },
             Ordering::Equal => {println!("You win!"); break;},
         }
     }
